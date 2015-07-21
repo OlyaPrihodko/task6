caba5b55640f4569b11cf8d04e5088404a16064d
OlyaPrihodko <olga.prixodko.94@mail.ru>
2015-07-21 14:53:25 +0300

    RequestParameterName второе пробное сохранение.

--- src/com/epam/prihodko/task6/controller/RequestParameterName.java
+++ src/com/epam/prihodko/task6/controller/RequestParameterName.java
@@ -5,4 +5,5 @@ public final class RequestParameterName {
     public static final String COMMAND_NAME = "command";
     public static final String PARAM_NAME_LOGIN = "login";
     public static final String PARAM_NAME_PASSWORD = "password";
+    public static final String TROLOLO = "trololo";
 }
